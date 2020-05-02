32904e9b1adab173abe7a49cf6cac6304bb44683
pjay79 <praveen.jayarajan@gmail.com>
2020-05-02 14:11:33 +1000

    refactor, add getContact

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -5,12 +5,15 @@ const allContacts = async (req, res) => {
 }
 
 const createContact = async (req, res) => {
-  try{
+  try {
     const { first_name, last_name, email, phone } = req.body;
-    const query = 'INSERT INTO contacts (first_name, last_name, email, phone) VALUES ($1, $2, $3, $4)';
+    const query = `
+      INSERT INTO contacts (first_name, last_name, email, phone) 
+      VALUES ($1, $2, $3, $4)
+    `;
     const { rows } = await pool.query(
       query,
-      [first_name, last_name, email, phone]
+      [first_name, last_name, email, phone],
     );
     res.send({
       status: 200,
@@ -31,6 +34,27 @@ const createContact = async (req, res) => {
 }
 
 const getContact = async (req, res) => {
+  try {
+    const { contactId } = req.params;
+    const query = `
+      SELECT * FROM contacts WHERE contact_id = $1
+    `;
+    const { rows } = await pool.query(
+      query,
+      [contactId],
+    );
+    res.send({
+      status: 200,
+      message: 'Contact retrieved',
+      data: rows[0],
+    });
+  } catch (err) {
+    res.send({
+      status: 404,
+      message: err.message,
+      data: null
+    });
+  }
 }
 
 const updateContact = async (req, res) => {
